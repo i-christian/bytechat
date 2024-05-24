0d0b9353eb7f6cc4bf402591ae4656f223cf37fc
Christian <111076892+i-christian@users.noreply.github.com>
2024-05-24 18:29:17 +0200

    refactor again

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use axum::{http::StatusCode, routing::get, Router};
-use sqlx::PgPool;
 
 async fn fallback() -> (StatusCode, &'static str) {
     (StatusCode::NOT_FOUND, "Not Found")
@@ -10,12 +9,7 @@ async fn hello_world() -> &'static str {
 }
 
 #[shuttle_runtime::main]
-async fn main(#[shuttle_shared_db::Postgres] db: PgPool) -> shuttle_axum::ShuttleAxum {
-    sqlx::migrate!()
-        .run(&db)
-        .await
-        .expect("Looks like something went wrong with migrations: (");
-
+async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .fallback(fallback);
