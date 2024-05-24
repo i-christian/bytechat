eb28b875591bb52495bfb9f012641456e50026aa
Christian <111076892+i-christian@users.noreply.github.com>
2024-05-24 15:45:08 +0200

    set up database migrations using sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{http::StatusCode, routing::get, Router};
+use sqlx::PgPool;
 
 async fn fallback() -> (StatusCode, &'static str) {
     (StatusCode::NOT_FOUND, "Not Found")
@@ -9,7 +10,12 @@ async fn hello_world() -> &'static str {
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(#[shuttle_shared_db::Postgres] db: PgPool) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!()
+        .run(&db)
+        .await
+        .expect("Looks like something went wrong with migrations: (");
+
     let router = Router::new()
         .route("/", get(hello_world))
         .fallback(fallback);
