bf555bca84170eadcec1a99957a94b825e0efa2b
Christian <111076892+i-christian@users.noreply.github.com>
2024-05-28 23:09:48 +0200

    Refactoring: - moved handlers out of main module

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,14 @@
+mod handlers;
+mod messages;
 mod state;
 
-use axum::{http::StatusCode, routing::get};
-use socketioxide::{
-    extract::{Data, SocketRef, State},
-    SocketIo,
-};
+use axum::routing::get;
+use handlers::{fallback, on_connect};
+use socketioxide::SocketIo;
 use sqlx::postgres::PgPoolOptions;
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
 use tracing::info;
-
-#[derive(Debug, serde::Deserialize)]
-struct MessageIn {
-    room: String,
-    text: String,
-}
-
-#[derive(serde::Serialize)]
-struct Messages {
-    messages: Vec<state::Message>,
-}
-
-/// Handles socket connection events
-///
-/// This function is called when a new socket connection is established. It sets up
-/// listeners for "join" and "message" events on the socket.
-///
-/// # Arguments
-///
-/// * `socket` - A reference to the connected socket
-async fn on_connect(socket: SocketRef) {
-    info!("socket connected: {}", socket.id);
-
-    socket.on(
-        "join",
-        |socket: SocketRef, Data::<String>(room), store: State<state::MessageStore>| async move {
-            info!("Received join: {:?}", room);
-            let _ = socket.leave_all();
-            let _ = socket.join(room.clone());
-            let messages = store.get(&room).await.unwrap_or_default();
-            let _ = socket.emit("messages", Messages { messages });
-        },
-    );
-
-    socket.on(
-        "message",
-        |socket: SocketRef, Data::<MessageIn>(data), store: State<state::MessageStore>| async move {
-            info!("Received message: {:?}", data);
-
-            let response = state::Message {
-                text: data.text,
-                user: format!("anon-{}", socket.id),
-                date: chrono::Utc::now(),
-            };
-
-            store.insert(&data.room, response.clone()).await.unwrap();
-
-            let _ = socket.within(data.room).emit("message", response);
-        },
-    )
-}
-
-/// Fallback handler for incorrect URLs
-///
-/// Returns a 404 Not Found status code and a "Not Found" message.
-///
-/// # Returns
-///
-/// A tuple containing the status code and a static string slice
-async fn fallback() -> (StatusCode, &'static str) {
-    (StatusCode::NOT_FOUND, "Not Found")
-}
 
 /// Main function to set up and run the server
 ///
@@ -85,6 +23,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .connect(&database_url)
         .await
         .expect("Failed to create pool.");
+
+    //sqlx::migrate!().run(&pool).await.expect("Migration failed");
 
     let messages = state::MessageStore::new(pool);
 
