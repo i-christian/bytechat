08f5e9a0edf50e1b7ca21df273a1874320854bb8
Christian <111076892+i-christian@users.noreply.github.com>
2024-05-29 00:43:50 +0200

    Refactor: Move route setup to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
 mod handlers;
 mod messages;
+mod routes;
 mod state;
 
-use axum::routing::get;
-use handlers::{fallback, on_connect};
-use socketioxide::SocketIo;
+use routes::setup_routes;
 use sqlx::postgres::PgPoolOptions;
-use tower::ServiceBuilder;
-use tower_http::cors::CorsLayer;
 use tracing::info;
 
-/// Main function to set up and run the server
-///
-/// This function sets up the message store, socket.io layer, and the axum application
-/// with routing and middleware. It starts the server using Shuttle runtime.
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
@@ -26,21 +19,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
 
     //sqlx::migrate!().run(&pool).await.expect("Migration failed");
 
-    let messages = state::MessageStore::new(pool);
-
-    let (layer, io) = SocketIo::builder().with_state(messages).build_layer();
-
-    io.ns("/", on_connect);
-
-    let app = axum::Router::new()
-        .fallback(fallback)
-        .route("/", get(|| async { "Hello, World!" }))
-        .with_state(io)
-        .layer(
-            ServiceBuilder::new()
-                .layer(CorsLayer::permissive())
-                .layer(layer),
-        );
+    let app = setup_routes(pool).await;
 
     info!("Starting server");
 
