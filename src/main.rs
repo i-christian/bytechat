75ff859a108310f9e0d054812034e48b165e47f3
Christian <111076892+i-christian@users.noreply.github.com>
2024-05-30 00:19:54 +0200

    Refactor: - Database initialisation using environment variables - Enable sqlx migrations for easy setup of database - Added dotenv dependancy crate to manage .env - Added .env file with a test DATABASE_URL

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ mod messages;
 mod routes;
 mod state;
 
+use dotenv::dotenv;
 use routes::setup_routes;
 use sqlx::postgres::PgPoolOptions;
+use std::env;
 use tracing::info;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -17,7 +20,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .await
         .expect("Failed to create pool.");
 
-    //sqlx::migrate!().run(&pool).await.expect("Migration failed");
+    sqlx::migrate!().run(&pool).await.expect("Migration failed");
 
     let app = setup_routes(pool).await;
 
