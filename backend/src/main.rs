7913fa993a6e952d20aa0110ca8ee3a0e98e887d
Christian <111076892+i-christian@users.noreply.github.com>
2024-10-18 22:52:26 +0200

    feat: add database to the app
    - set up database url in .env

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,9 +3,9 @@ mod messages;
 mod routes;
 mod state;
 
+use sqlx::postgres::PgPoolOptions;
 use dotenv::dotenv;
 use routes::setup_routes;
-use sqlx::postgres::PgPoolOptions;
 use std::env;
 use tracing::info;
 
@@ -16,16 +16,16 @@ async fn main() {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     let pool = PgPoolOptions::new()
-        .max_connections(5)
+        .max_connections(10)
         .connect(&database_url)
         .await
-        .expect("Failed to create pool.");
+        .expect("Failed to create a database connection.");
 
     sqlx::migrate!().run(&pool).await.expect("Migration failed");
 
     let app = setup_routes(pool).await;
 
     info!("Starting server");
-
-    Ok(app.into())
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.expect("Failed to bind port");
+    axum::serve(listener, app).await.expect("Failed to start application");
 }
