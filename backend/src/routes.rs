1a2a9c36ce2f2cedd9f0e5dd96c5d940b183a175
Christian <111076892+i-christian@users.noreply.github.com>
2024-10-22 22:17:38 +0200

    remove files
    - removed the messages, state and handlers files
    - To recreate a better backend

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,7 +1,4 @@
-use crate::handlers::{fallback, on_connect};
-use crate::state::MessageStore;
 use axum::{routing::get, Router};
-use socketioxide::SocketIo;
 use sqlx::postgres::PgPool;
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
@@ -16,18 +13,6 @@ use tower_http::cors::CorsLayer;
 ///
 /// An Axum Router configured with routes, middleware, and state.
 pub async fn setup_routes(pool: PgPool) -> Router {
-    let messages = MessageStore::new(pool);
-
-    let (layer, io) = SocketIo::builder().with_state(messages).build_layer();
-    io.ns("/", on_connect);
-
     Router::new()
-        .fallback(fallback)
-        .route("/", get(|| async { "Hello, World!" }))
-        .with_state(io)
-        .layer(
-            ServiceBuilder::new()
-                .layer(CorsLayer::permissive())
-                .layer(layer),
-        )
+        .route("/", get(|| async { "Hello, World!" }));
 }
