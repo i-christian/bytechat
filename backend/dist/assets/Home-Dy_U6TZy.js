c6fae3c5f9ea03139a710873976b166f9f7d62c3
Christian <111076892+i-christian@users.noreply.github.com>
2024-11-06 01:14:23 +0200

    feat: implement register user frontend page

---
+++ backend/dist/assets/Home-Dy_U6TZy.js
@@ -0,0 +1,1 @@
+import{t as a}from"./index-BiNU-a0o.js";var e=a("<main><aside>chatrooms</aside><p>messages");const s=()=>e();export{s as default};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Router } from "@solidjs/router";
 import { Component, lazy } from "solid-js";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
+import Register from "./pages/Register";
 
 const Home: Component = lazy(() => import("./pages/Home"));
 const Forbidden: Component = lazy(() => import("./pages/403"));
@@ -12,6 +13,7 @@ const App: Component = () => {
   return (
     <Router>
       <Route path="/" component={Home} />
+      <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
       <Route path="/logout" component={Logout} />
       <Route path="/403" component={Forbidden} />
