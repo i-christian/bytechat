f250a2491afa4e99bd73c99b300cca910cc3ff18
christian <111076892+i-christian@users.noreply.github.com>
2025-04-14 14:11:02 +0200

    fix(db): correct `GetPrivateRooms` query logic
    Replaces faulty subquery logic with standard JOINs
    to accurately find users sharing private rooms with a target user.
    
    Handles cases where the target user is in multiple private rooms.

--- internal/server/sql/queries/rooms.sql
+++ internal/server/sql/queries/rooms.sql
@@ -2,26 +2,26 @@
 select * from create_private_room($1, $2); 
 
 -- name: GetPrivateRooms :many
-select 
-    users.first_name || ' ' || users.last_name as full_name,
-    users.status,
-    users.user_id,
-    users.updated_at,
-    chat_rooms.room_id
-from users
-join chat_rooms using(user_id)
-join rooms using (room_id)
-where rooms.room_id = (
-    select 
-        rooms.room_id
-    from users 
-    join chat_rooms using(user_id)
-    join rooms using(room_id)
-    where users.user_id = $1
-        and rooms.room_type = 'private'
-    order by rooms.room_id
-)
-order by users.updated_at desc;
+select
+    usr.first_name || ' ' || usr.last_name as full_name,
+    usr.status,
+    usr.user_id,
+    usr.updated_at,
+    shared_cr.room_id
+from
+    chat_rooms as specific_user_cr
+join
+    rooms as r on specific_user_cr.room_id = r.room_id
+join
+    chat_rooms as shared_cr on r.room_id = shared_cr.room_id
+join
+    users as usr on shared_cr.user_id = usr.user_id
+where
+    specific_user_cr.user_id = $1
+    and r.room_type = 'private'
+    and usr.user_id <> $1
+order by
+    usr.updated_at desc;
 
 -- name: CreatePublicRoom :exec
 insert into rooms(name, description, room_type) 
